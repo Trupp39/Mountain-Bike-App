89933c1a570d881072931e7925f60a2671d26ffa
administrator <administrator@IQ-MBP13s-MacBook-Pro.local>
2019-10-22 12:28:31 -0400

    started setting up project in xcode

---
+++ Mountain Bike App/Mountain Bike AppTests/Mountain_Bike_AppTests.swift
@@ -0,0 +1,36 @@
+//
+//  Mountain_Bike_AppTests.swift
+//  Mountain Bike AppTests
+//
+//  Created by administrator on 10/22/19.
+//  Copyright © 2019 administrator. All rights reserved.
+//
+
+import XCTest
+@testable import Mountain_Bike_App
+
+class Mountain_Bike_AppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
