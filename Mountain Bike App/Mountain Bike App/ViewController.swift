882787505eb82dc7a0701455d19bfa7234c667f9
administrator <administrator@IQ-MBP13s-MacBook-Pro.local>
2019-10-29 13:02:25 -0400

    moved the launch story board to main story board and added the hooks for the UI elements to the view controller... slowly but surely progress will be made at lunch

--- Mountain Bike App/Mountain Bike App/ViewController.swift
+++ Mountain Bike App/Mountain Bike App/ViewController.swift
@@ -9,7 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+//MARK: Properties
+    
+    @IBOutlet weak var rideNameLabel:
+        UILabel!
+    
+    @IBOutlet weak var rideNameText:
+        UITextField!
+    
+    @IBOutlet weak var ratingLabel:
+        UILabel!
+    
+    @IBOutlet weak var ratingSlider:
+        UISlider!
+    
+    @IBOutlet weak var ridersLabel:
+        UILabel!
+    
+    @IBOutlet weak var ridersText:
+        UITextField!
+    
+    @IBOutlet weak var commentsLabel:
+        UILabel!
+    
+    @IBOutlet weak var commentsText: UITextField!
+    
+    @IBOutlet weak var viewRidesButton: UIButton!
+    
+    @IBOutlet weak var sendItButton:
+        UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
