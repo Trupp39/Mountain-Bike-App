c76a2ce9d1729e141fc56fd00b19b80c56508607
administrator <administrator@IQ-MBP13s-MacBook-Pro.local>
2019-11-04 13:55:05 -0500

    flustering day

---
+++ Mountain Bike App/Mountain Bike App/FlowControl.swift
@@ -0,0 +1,28 @@
+import UIKit
+
+class FlowControl: UIStackView {
+    //MARK: Initialization
+    override init(frame: CGRect) {
+        super.init(frame: frame);
+        setupButtons();
+    }
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder);
+        setupButtons();
+    }
+    
+    //MARK: Private Methods
+    private func setupButtons() {
+        let button = UIButton();
+        button.backgroundColor = UIColor.green;
+        
+        // Add constraints
+        button.translatesAutoresizingMaskIntoConstraints = false;
+        button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true;
+        button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true;
+        
+        // Add the button to the stack
+        addArrangedSubview(button);
+    }
+}
