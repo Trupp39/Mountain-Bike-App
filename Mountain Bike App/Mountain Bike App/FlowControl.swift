957df4ddef46293a4222664e78eb40a623eee394
administrator <administrator@IQ-MBP13s-MacBook-Pro.local>
2019-11-05 13:11:47 -0500

    fixing up the ui ever so slightly and began on the logic work of the custom flow control

--- Mountain Bike App/Mountain Bike App/FlowControl.swift
+++ Mountain Bike App/Mountain Bike App/FlowControl.swift
@@ -2,6 +2,8 @@ import UIKit
 
 class FlowControl: UIStackView {
     //MARK: Initialization
+    private var flowButtons = [UIButton]();
+    var flow = 4;
     override init(frame: CGRect) {
         super.init(frame: frame);
         setupButtons();
@@ -14,15 +16,25 @@ class FlowControl: UIStackView {
     
     //MARK: Private Methods
     private func setupButtons() {
-        let button = UIButton();
-        button.backgroundColor = UIColor.green;
+        for _ in 0...3 {
+            let button = UIButton();
+            button.backgroundColor = UIColor.green;
         
-        // Add constraints
-        button.translatesAutoresizingMaskIntoConstraints = false;
-        button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true;
-        button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true;
+            // Add constraints
+            button.translatesAutoresizingMaskIntoConstraints = false;
+            button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true;
+            button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true;
         
-        // Add the button to the stack
-        addArrangedSubview(button);
+            button.addTarget(self, action: #selector(FlowControl.flowButtonPress(button:)), for: .touchUpInside)
+        
+            // Add the button to the stack
+            addArrangedSubview(button);
+            
+            flowButtons.append(button);
+        }
+    }
+    
+    @objc func flowButtonPress(button: UIButton) {
+        print("Button pressed 👍")
     }
 }
